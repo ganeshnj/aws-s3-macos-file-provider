7ab184c9243b2335a3143951ae6dac96df968d3e
Ganesh Jangir <ganeshnj@outlook.com>
2021-10-03 17:34:35 -0700

    add macOS app project

---
+++ AWSS3macOSFileProvider/AWSS3macOSFileProvider/AWSS3macOSFileProviderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWSS3macOSFileProviderApp.swift
+//  AWSS3macOSFileProvider
+//
+//  Created by Ganesh Jangir on 10/3/21.
+//
+
+import SwiftUI
+
+@main
+struct AWSS3macOSFileProviderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
